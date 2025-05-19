f2dfe4a726b9d2a6c9920242bacd9ab98153be6f
MetacomicSolutions <128888808+MetacomicSolutions@users.noreply.github.com>
2025-05-19 16:30:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.error('Service Worker error:', err));
+  });
+}
+
+
